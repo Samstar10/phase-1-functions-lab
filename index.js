68b1783710189f562725be5b28bec961b07b7107
Samuel Muli <samuelmuli95@gmail.com>
2023-09-19 11:49:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42)
+}
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    const blocksTravelled = Math.abs(destination - start)
+    return blocksTravelled * 264
+}
+
+function calculatesFarePrice(start, destination){
+    let feetTravelled = distanceTravelledInFeet(start, destination)
+
+    if(feetTravelled <= 400){
+        return 0
+    }else if(feetTravelled > 400 && feetTravelled <= 2000){
+        return (feetTravelled - 400) * 0.02
+    }else if(feetTravelled > 2000 && feetTravelled <= 2500){
+        return 25
+    }else if(feetTravelled > 2500){
+        return 'cannot travel that far'
+    }
+}
